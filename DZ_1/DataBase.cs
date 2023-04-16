292913c940d7747d4d2826f4a6224c9798427993
Gambaryan <hrachruby@gmail.com>
2023-04-16 19:25:15 +0300

    Добавьте файлы проекта.

---
+++ DZ_1/DataBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace DZ_1
+{
+    internal class DataBase
+    {
+        SqlConnection db_con = new SqlConnection(@"Data Source = GAMBARYAN\STP; Initial Catalog = DZ_1; Integrated Security = True");
+
+        public void openConnection()
+        {
+            if (db_con.State == System.Data.ConnectionState.Closed)
+            {
+                db_con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (db_con.State == System.Data.ConnectionState.Open)
+            {
+                db_con.Close();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return db_con;
+        }
+    }
+}
---
+++ DZ_1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace DZ_1
+{
+    public partial class Form1 : Form
+    {
+        DataBase db = new DataBase();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"select * from Tovar;";
+            SqlCommand command = new SqlCommand(querystring, db.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+
+        }
+
+        private void button_zapusk_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Csv файлы(*.csv)|*.csv|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(saveFileDialog.FileName, false, Encoding.Unicode);
+                try
+                {
+                    List<int> col_n = new List<int>();
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                        if (col.Visible)
+                        {
+                            //sw.Write(col.HeaderText + "\t");
+                            col_n.Add(col.Index);
+                        }
+                    //sw.WriteLine();
+                    int x = dataGridView1.RowCount;
+                    if (dataGridView1.AllowUserToAddRows) x--;
+
+                    for (int i = 0; i < x; i++)
+                    {
+                        for (int y = 0; y < col_n.Count; y++)
+                            sw.Write(dataGridView1[col_n[y], i].Value + "\t");
+                        sw.Write(" \r\n");
+                    }
+                    sw.Close();
+
+                    MessageBox.Show("Файл успешно сохранён", "Оповещение");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                }
+
+            }
+        }
+    }
+}
